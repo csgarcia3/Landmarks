cbee710f723ad79de741c33c21c90145537b49d5
Chelsea <csgarcia3@cps.edu>
2023-03-02 13:29:16 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Garcia, Chelsea S on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
