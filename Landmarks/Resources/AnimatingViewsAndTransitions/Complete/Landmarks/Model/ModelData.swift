ec4aa4fd61f2234059ac378eb618c3026c7a1454
Chelsea <csgarcia3@cps.edu>
2023-04-10 13:15:12 -0500

    April 10

--- Landmarks/Resources/AnimatingViewsAndTransitions/Complete/Landmarks/Model/ModelData.swift
+++ Landmarks/Resources/AnimatingViewsAndTransitions/Complete/Landmarks/Model/ModelData.swift
@@ -1,3 +1,10 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Storage for model data.
+*/
+
 import Foundation
 import Combine
 
@@ -8,18 +15,18 @@ final class ModelData: ObservableObject {
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
     do {
-     data = try Data(contentsOf: file)
+        data = try Data(contentsOf: file)
     } catch {
-        fatalError("Couldn't load \(filename) form main bundle:\n\(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
@@ -27,3 +34,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
--- Landmarks/Resources/AnimatingViewsAndTransitions/StartingPoint/Landmarks/Model/ModelData.swift
+++ Landmarks/Resources/AnimatingViewsAndTransitions/StartingPoint/Landmarks/Model/ModelData.swift
@@ -1,25 +1,31 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Storage for model data.
+*/
+
 import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
-    var hikes: [Hike] = load("hikeData.json")
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
     do {
-     data = try Data(contentsOf: file)
+        data = try Data(contentsOf: file)
     } catch {
-        fatalError("Couldn't load \(filename) form main bundle:\n\(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
@@ -27,3 +33,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
